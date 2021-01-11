17f997df0e96e3ff9a96e24c1deffbe229ce1f58
sgiovannini <santiagogiovannini@gmail.com>
2021-01-11 11:58:38 -0300

    Updating provisioning scripts

--- scripts/provisioning/03.ApplyTemplate-DEVBF2019.ps1
+++ scripts/provisioning/03.ApplyTemplate-DEVBF2019.ps1
@@ -1,6 +1,12 @@
 $siteUrl = "https://devbf2019.sharepoint.com/sites/RBPromociones"
-$UserName = "adminshp@devbf2019.onmicrosoft.com"
-$pwd = "baufest_123"
+
+#Admin
+#$UserName = "adminshp@devbf2019.onmicrosoft.com"
+#$pwd = "baufest_123"
+
+#User
+$UserName = "baufest1@devbf2019.onmicrosoft.com"
+$pwd = "test_123"
 
 $Credentials = New-Object System.Management.Automation.PSCredential ($UserName, (ConvertTo-SecureString $pwd -AsPlainText -Force))
 Connect-PnPOnline -Url $siteUrl –Credentials $Credentials
