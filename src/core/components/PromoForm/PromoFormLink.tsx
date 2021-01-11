3c0e2f3922c1f4da49e3ea7bb14b2e15bfd02a40
sgiovannini <santiagogiovannini@gmail.com>
2021-01-11 13:15:39 -0300

    Adding the link to the mockup

--- src/core/components/PromoForm/PromoFormLink.tsx
+++ src/core/components/PromoForm/PromoFormLink.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { ExtensionContext } from "@microsoft/sp-extension-base";
 import { WebPartContext } from "@microsoft/sp-webpart-base";
 import { PromoFormDialog } from '.';
+import { MockupFormDialog } from '../MockupFormDialog/MockupFormDialog';
 import { TextField } from "office-ui-fabric-react";
 import { CommonHelper } from "../../common/CommonHelper";
 
@@ -15,6 +16,10 @@ export class PromoFormLink extends React.Component<IPromoFormLinkProps, {}> {
         var output = 
             <div>
                 <a onClick={() => this.openPromoFormDialog()} style={{cursor: "pointer" }}>Nueva promoción</a>
+
+                <br/><br/>
+
+                {/* <a onClick={() => this.openMockupFormDialog()} style={{cursor: "pointer" }}>Maquetado</a> */}
             </div>;
 
         return output;
@@ -27,4 +32,12 @@ export class PromoFormLink extends React.Component<IPromoFormLinkProps, {}> {
         dialog.context = this.props.context;
         dialog.show();
     }
+
+    // private openMockupFormDialog(): void{
+    // {
+    //     var dialog: MockupFormDialog = new MockupFormDialog();
+    //     dialog.context = this.props.context;
+    //     dialog.show();
+    // }
+}
 }
