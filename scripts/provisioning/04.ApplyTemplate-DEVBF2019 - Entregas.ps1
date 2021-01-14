c34a216748a94b93f30f3af95413d2a0aabe3a1f
sgiovannini <santiagogiovannini@gmail.com>
2021-01-14 13:23:39 -0300

    Adding deployment script for client's site

---
+++ scripts/provisioning/04.ApplyTemplate-DEVBF2019 - Entregas.ps1
@@ -0,0 +1,14 @@
+$siteUrl = "https://devbf2019.sharepoint.com/sites/Entregas-RBPromociones"
+
+#Admin
+$UserName = "adminshp@devbf2019.onmicrosoft.com"
+$pwd = "baufest_123"
+
+#User
+#$UserName = "baufest1@devbf2019.onmicrosoft.com"
+#$pwd = "test_123"
+
+$Credentials = New-Object System.Management.Automation.PSCredential ($UserName, (ConvertTo-SecureString $pwd -AsPlainText -Force))
+Connect-PnPOnline -Url $siteUrl –Credentials $Credentials
+
+Apply-PnPProvisioningTemplate -Path template.xml -Handlers Lists
