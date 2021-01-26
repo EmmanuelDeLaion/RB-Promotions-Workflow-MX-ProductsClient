adcb0b6a56a16f4d301a70c1ccd3b826b2e4273d
sgiovannini <santiagogiovannini@gmail.com>
2021-01-26 16:04:00 -0300

    Removing field customizer

--- src/extensions/promoFormListView/components/IPromoFormListViewProps.tsx
+++
@@ -1,7 +0,0 @@
-import { ExtensionContext } from "@microsoft/sp-extension-base";
-import { WebPartContext } from "@microsoft/sp-webpart-base";
-
-export interface IPromoFormListViewProps {
-    itemId: string;
-    context: ExtensionContext | WebPartContext;  
-  }
